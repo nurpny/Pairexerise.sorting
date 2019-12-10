314292842583327fb2f7fbffffb5e766bd390d78
nurpny <runnurrr@gmail.com>
2019-12-10 17:57:16 -0500

    Completed exercises

--- bubblesort.js
+++ bubblesort.js
@@ -1,29 +1,31 @@
 function bubbleSort(array) {
-
-  let solution = array
-
-  if (array.length ===0 || array.length ===1){
-    return array  
-  }
-  for(let i =0 ; i< solution.length; i++){
-    for(let j=0; j <solution.length -i; j++){
-
-
-    //if a swap needs to happen
-    if (solution[j]> solution[j+1]) { 
-      swap()
-      let biggerValue = solution[j]
-      solution[j] = solution[j+1]
-      solution[j+1] = biggerValue
+    for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - i - 1; j++) {
+      //if a swap needs to happen
+      if (array[j] > array[j + 1]) {
+        swap(array, j);
+      }
     }
-    console.log(solution, i)
   }
-  }
-  return solution
+  return array
 }
 
-function swap(){
-  return 
+function bubbleSortRecurse(array) {
+  if (array.length <= 1) {
+    return array;
+  }
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > array[i + 1]) {
+      swap(array, i);
+    }
+  }
+  return bubbleSort(array.slice(0, array.length - 1)).concat(array[array.length - 1])
+}
+
+function swap(anArr, j) {
+  const oldJ = anArr[j]
+  anArr[j] = anArr[j + 1]
+  anArr[j + 1] = oldJ
 }
 
 
--- mergesort.js
+++ mergesort.js
@@ -1,38 +1,57 @@
 function split(wholeArray) {
-
-const len = wholeArray.length
-let firstHalf=wholeArray.slice (0,Math.floor(len/2))
-let secondHalf=wholeArray.slice(Math.floor(len/2))
-
+  const len = wholeArray.length
+  let firstHalf = wholeArray.slice(0, Math.floor(len / 2))
+  let secondHalf = wholeArray.slice(Math.floor(len / 2))
   return [firstHalf, secondHalf];
 }
 
 
-function merge(array1, array2){
+function merge(array1, array2) {
 
-  let arr1 =[...array1]
-  let arr2 = [...array2]
-  let solution =[]
-
-while (arr1.length >0 || arr2.length >0){
-  if(arr1[0] > arr2[0] || arr1.length === 0){
-    solution.push(arr2[0])
-    arr2= arr2.slice(1)
-  }else{
-    solution.push(arr1[0])
-    arr1= arr1.slice(1)
+  const sortedArr = new Array(array1.length + array2.length);
+  let k = 0;
+  let i = 0;
+  let j = 0;
+  while (i < array1.length && j < array2.length) {
+    if (array1[i] <= array2[j]) {
+      sortedArr[k++] = array1[i++]
+    } else {
+      sortedArr[k++] = array2[j++];
+    }
   }
+  while (i < array1.length) {
+    sortedArr[k++] = array1[i++];
+  }
+  while (j < array2.length) {
+    sortedArr[k++] = array2[j++];
+  }
+  return sortedArr;
 }
 
+
+function mergeOrig(array1, array2) {
+  // this increases runtime error due to array.slice methods
+  let arr1 = [...array1]
+  let arr2 = [...array2]
+  let solution = []
+
+  while (arr1.length > 0 || arr2.length > 0) {
+    if (arr1[0] > arr2[0] || arr1.length === 0) {
+      solution.push(arr2[0])
+      arr2 = arr2.slice(1)
+    } else {
+      solution.push(arr1[0])
+      arr1 = arr1.slice(1)
+    }
+  }
   return solution
 }
 
 function mergeSort(array) {
-
-  if (array.length <= 1){
+  if (array.length <= 1) {
     return array
   }
-let [firstHalf, secondHalf] =split(array)
-return merge(mergeSort(firstHalf), mergeSort(secondHalf))
+  let [firstHalf, secondHalf] = split(array)
+  return merge(mergeSort(firstHalf), mergeSort(secondHalf))
 }
 
